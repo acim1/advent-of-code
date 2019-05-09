12bee8d57ece4369be783370b323b435170587ca
acim1 <mkeller02@gmail.com>
2019-05-09 15:14:17 -0500

    Finished day 16 2017 pt1

---
+++ src/advent_of_code/day16_17_pt1.clj
@@ -0,0 +1,46 @@
+(ns advent-of-code.day16-17-pt1
+  (:require [clojure.java.io :as io]))
+
+(defn spin [programs n]
+  (let [[front back] (split-at (- (count programs) n) (vals programs))]
+    (into (sorted-map) (zipmap (range 0 (count programs)) (concat back front)))))
+
+(defn exchange [programs i j]
+  (-> programs
+      (assoc i (programs j))
+      (assoc j (programs i))))
+
+(defn partner [programs a b]
+  (let [programs-inverted (clojure.set/map-invert programs)]
+    (-> programs
+        (assoc (programs-inverted a) b)
+        (assoc (programs-inverted b) a))))
+
+(defn parse-instructions [instructions]
+  (for [instr instructions]
+    (let [[_ n :as sX]    (re-matches #"s(\d+)" instr)
+          [_ i j :as xAB] (re-matches #"x(\d+)\/(\d+)" instr)
+          [_ a b :as pAB] (re-matches #"p([a-z])\/([a-z])" instr )]
+      (cond
+        sX  #(spin % (Integer/parseInt n))
+        xAB #(exchange % (Integer/parseInt i) (Integer/parseInt j))
+        pAB #(partner % (first a) (first b))
+        :else (throw (IllegalArgumentException. (str "No instruction match for: " instr)))))))
+
+(def test-input [(into (sorted-map) (zipmap (range 0 16) "abcde"))
+                 (parse-instructions (clojure.string/split "s1,x3/4,pe/b" #","))])
+
+(defn input []
+  [(into (sorted-map) (zipmap (range 0 16) "abcdefghijklmnop"))
+   (-> (io/resource "day16-17.txt")
+       slurp
+       (clojure.string/split #",")
+       parse-instructions)])
+
+(defn permutation-promenade [[orig-programs orig-instructions]]
+  (loop [programs orig-programs [instr & instructions] orig-instructions]
+    (if-not instr
+      [programs orig-instructions]
+      (recur (instr programs) instructions))))
+        
+
