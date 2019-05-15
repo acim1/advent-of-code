34b432c3bed02ab3f86862631f28134fa62d57bf
acim1 <mkeller02@gmail.com>
2019-05-15 13:49:19 -0500

    Finish day 18

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject advent-of-code "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :source-paths ["src/main/clj"]
   :java-source-paths ["src/main/java"]
   :resource-paths ["src/main/resources"])
--- src/main/clj/advent_of_code/day18_17_pt1.clj
+++ src/main/clj/advent_of_code/day18_17_pt1.clj
@@ -2,16 +2,13 @@
   (:require [clojure.java.io :as io]))
 
 (def test-input
-  (-> "set a 1
-add a 2
-mul a a
-mod a 5
-snd a
-set a 0
+  (-> "snd 1
+snd 2
+snd p
 rcv a
-jgz a -1
-set a 1
-jgz a -2"
+rcv b
+rcv c
+rcv d"
       (clojure.string/split-lines)))
 
 (def input
@@ -59,30 +56,59 @@ jgz a -19"
       (clojure.string/split-lines)))
 
 ;; just starts with PC address; we'll add :snd and :rcv for last snd and rcv, and registers as we go
-(def initial-vm {:pc 0}) 
+(def initial-prog {:pc 0 :status :run :snd-cnt 0}) 
 
-(defn val [vm x]
+(defn val [prog x]
   (if (re-matches #"[a-z]" x)
-    (get vm x 0)
+    (get prog x 0)
     (Long/parseLong x)))
 
 (defn parse-instruction [i]
   (if-let [[_ op x y] (re-matches #"(snd|set|add|mul|mod|rcv|jgz) ([a-z]|-?\d+) ?([a-z]|-?\d+)?" i)]
     (case op
-      "snd" #(assoc % :snd (val % x))
+      "snd" #(do
+               (swap! (:snd %) (fn [q] (conj q (val % x))))
+               (update % :snd-cnt inc))
       "set" #(assoc % x (val % y))
       "add" #(update % x (fnil (partial + (val % y)) 0))
       "mul" #(update % x (fnil (partial * (val % y)) 0))
       "mod" #(update % x (fnil (fn [xv] (mod xv (val % y))) 0))
-      "rcv" #(if-not (zero? (val % x)) (assoc % :rcv (:snd %)) %)
+      "rcv" #(if (peek @(:rcv %))
+               (-> %
+                   (assoc x (-> (swap-vals! (:rcv %) pop) first peek))
+                   (assoc :status :run))
+               (assoc % :status :wait))
       "jgz" #(if (pos? (val % x)) (update % :pc (fnil (partial + (dec (val % y))) 0)) %))
     (throw (IllegalArgumentException. (format "Cannot parse instruction '%s'" i)))))
 
+(defn run-prog [prog xs]
+  (cond
+    (= :terminated (:status prog))
+    prog
+
+    (not (<= 0 (inc (:pc prog)) (dec (count xs))))
+    (assoc prog :status :terminated)
+
+    :else
+    (-> prog
+        (#(if-not (= :wait (:status prog)) (update % :pc inc) %))
+        (#(let [x (xs (:pc %))]
+            (x %))))))
+
 (defn duet [instructions]
-  (let [xs (mapv parse-instruction instructions)]
-    (loop [vm ((first xs) initial-vm)]
-      (or (:rcv vm)
-          (recur (-> vm
-                     (update :pc inc)
-                     (#(let [x (xs (:pc %))]
-                         (x %)))))))))
+  (let [prog1s (atom [])
+        xs (mapv parse-instruction instructions)
+        rcv0 (atom clojure.lang.PersistentQueue/EMPTY)
+        rcv1 (atom clojure.lang.PersistentQueue/EMPTY)]
+    (loop [prog0 ((first xs) (-> initial-prog
+                                 (assoc "p" 0)
+                                 (assoc :rcv rcv0)
+                                 (assoc :snd rcv1)))
+           prog1 ((first xs) (-> initial-prog
+                                 (assoc "p" 1)
+                                 (assoc :rcv rcv1)
+                                 (assoc :snd rcv0)))]
+      (or (and (#{:wait :terminated} (:status prog0))
+               (#{:wait :terminated} (:status prog1))
+               (:snd-cnt prog1))
+          (recur (run-prog prog0 xs) (run-prog prog1 xs))))))
